0bd9451c76b7ddd2e2fbceb4ae155d696190a354
poojanavkar <38310069+poojanavkar@users.noreply.github.com>
2020-12-28 17:10:51 +0530

    called uniquequery

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,7 @@
+var callgetdistict = require("./getdistinct.js");
+Parse.Cloud.define("UniqueQuery", callgetdistict.getdistinctFunction);
+
+
 Parse.Cloud.define('hello', function(req, res) {
   return 'Hi';
 });
